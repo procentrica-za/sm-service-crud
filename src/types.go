bf2bb2dbdb375661c5bf19f69adab38d4a26ffe0
hsgilliland <manie.gilliland@gmail.com>
2020-03-09 09:01:16 +0200

    added calls for messaging

--- src/types.go
+++ src/types.go
@@ -483,6 +483,53 @@ type CardImageBatchRequest struct {
 	Cards []CardImageRequest `json:"cards"`
 }
 
+//Messaging types
+type StartChat struct {
+	SellerID string `json:"sellerid"`
+	BuyerID  string `json:"buyerid"`
+}
+
+type StartChatResult struct {
+	ChatPosted bool   `json:"chatposted"`
+	ChatID     string `json:"chatid"`
+	Message    string `json:"message"`
+}
+
+type ChatID struct {
+	ChatID string `json:"id"`
+}
+
+type DeleteChatResult struct {
+	ChatDeleted bool   `json:"chatposted"`
+	Message     string `json:"message"`
+}
+
+type GetActiveChatResult struct {
+	ChatID   string `json:"chatid"`
+	UserName string `json:"username"`
+}
+
+type ActiveChatList struct {
+	ActiveChats []GetActiveChatResult `json:"activechats"`
+}
+
+type GetMessageResult struct {
+	MessageID   string `json:"messageid"`
+	UserName    string `json:"username"`
+	Message     string `json:"message"`
+	MessageDate string `json:"messagedate"`
+}
+
+type MessageList struct {
+	Messages []GetMessageResult `json:"messages"`
+}
+
+type SendMessage struct {
+	ChatID   string `json:"chatid"`
+	AuthorID string `json:"authorid"`
+	Message  string `json:"message"`
+}
+
 type Config struct {
 	ListenServePort string
 }
